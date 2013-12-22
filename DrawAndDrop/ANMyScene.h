2a6ac5d78e19f17d6038ad3dbf5e06cbeaa424f3
Alex Nichol <alexnichol@comcast.net>
2013-12-22 11:20:56 -0500

    Initial Commit

---
+++ DrawAndDrop/ANMyScene.h
@@ -0,0 +1,13 @@
+//
+//  ANMyScene.h
+//  DrawAndDrop
+//
+
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ANMyScene : SKScene
+
+@end
---
+++ DrawAndDrop/ANViewController.h
@@ -0,0 +1,14 @@
+//
+//  ANViewController.h
+//  DrawAndDrop
+//
+
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ANViewController : UIViewController
+
+@end
